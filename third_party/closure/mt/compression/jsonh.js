a89d69bbc41243d03e6a1bd608dd26ac325e0b96
robin <obin.mf@gmail.com>
2012-01-21 21:39:32 +0700

    make googy to jsonh and add unittest

--- third_party/closure/mt/compression/jsonh.js
+++ third_party/closure/mt/compression/jsonh.js
@@ -1,4 +1,9 @@
 /**
+ * Copyright (C) 2012 Ansvia Inc.
+ * Ported to closure-library based on Andrea Giamarchi's code.
+ * Author: Robin Syihab.
+ */
+/**
  * Copyright (C) 2011 by Andrea Giammarchi, @WebReflection
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,317 +25,125 @@
  * THE SOFTWARE.
  */
 
-goog.provide("mt.compression.Jsonh")
-
+goog.provide("mt.compression.Jsonh");
+goog.require("goog.array");
 
 /**
  * @constructor
  */
-mt.compression.Jsonh = function(){
+mt.compression.Jsonh = function(list){
+	this.list_ = list;
+};
+
+/**
+ * @private
+ */
+mt.compression.Jsonh.prototype.objectKeys_ = function(obj){
+	var Object_keys = Object.keys || function (o) {
+        var keys = [], key;
+        for (key in o) o.hasOwnProperty(key) && keys.push(key);
+        return keys;
+    }
+	return Object_keys(obj);
+};
+
+/**
+ * @private
+ */
+mt.compression.Jsonh.prototype.hpack_ = function(list){
+	var length = list.length,
+		keys = this.objectKeys_(length ? list[0] : {}),
+		klength = keys.length,
+		result = Array(length * klength),
+		i, j = 0, ki, o;
 	
+	for(i = 0; i < length; ++i){
+		o = list[i];
+		for ( ki = 0; ki < klength; ki++ ){
+			result[j++] = o[keys[ki]];
+		}
+	}
+	return goog.array.concat([klength], keys, result);
+};
+
+/**
+ * @private
+ */
+mt.compression.Jsonh.prototype.hunpack_ = function(hlist){
+	var length = hlist.length,
+		klength = hlist[0],
+		result = Array(((length - klength - 1) / klength) || 0),
+		i = 1 + klength,
+		j = 0, z,
+		ki;
 	
-    // transforms [{a:"A"},{a:"B"}] to [1,"a","A","B"]
-    function hpack(list) {
-        for (var
-            length = list.length,
-            // defined properties (out of one object is enough)
-            keys = Object_keys(length ? list[0] : {}),
-            klength = keys.length,
-            // static length stack of JS values
-            result = Array(length * klength),
-            i = 0,
-            j = 0,
-            ki, o;
-            i < length; ++i
-        ) {
-            for (
-                o = list[i], ki = 0;
-                ki < klength;
-                result[j++] = o[keys[ki++]]
-            );
-        }
-        // keys.length, keys, result
-        return concat.call([klength], keys, result);
-    }
+	for (; i < length; ){
+		z = {};
+		result[j++] = z;
+		for(ki = 0; ki < klength; ){
+			z[hlist[++ki]] = hlist[i++];
+		}
+	}
+	return result;
+};
 
-    // transforms [1,"a","A","B"] to [{a:"A"},{a:"B"}]
-    function hunpack(hlist) {
-        for (var
-            length = hlist.length,
-            klength = hlist[0],
-            result = Array(((length - klength - 1) / klength) || 0),
-            i = 1 + klength,
-            j = 0,
-            ki, o;
-            i < length;
-        ) {
-            for (
-                result[j++] = (o = {}), ki = 0;
-                ki < klength;
-                o[hlist[++ki]] = hlist[i++]
-            );
-        }
-        return result;
-    }
+/**
+ * @private
+ */
+mt.compression.Jsonh.prototype.iteratingWith_ = function(method){
+	return function(item){
+		var path = this,
+			current = item,
+			i, 
+			length = path.length,
+			j, k, tmp;
+		for(i=0; i < length; ++i){
+			k = path[i];
+			tmp = current[k];
+			if( isArray(tmp) ){
+				j = i + 1;
+				current[k] = j < length ? goog.array.map(tmp, method, path.slice(j)) : method(tmp);
+			}
+			current = current[k];
+		}
+		return item;
+	};
+};
 
-    // recursive: called via map per each item h(pack|unpack)ing each entry through the schema
-    function iteratingWith(method) {
-        return function iterate(item) {
-            for (var
-                path = this,
-                current = item,
-                i = 0, length = path.length,
-                j, k, tmp;
-                i < length; ++i
-            ) {
-                if (isArray(tmp = current[k = path[i]])) {
-                    j = i + 1;
-                    current[k] = j < length ?
-                        map.call(tmp, through, path.slice(j)) :
-                        method(tmp)
-                    ;
-                }
-                current = current[k];
-            }
-            return item;
-        };
-    }
+/**
+ * @private
+ */
+mt.compression.Jsonh.prototype.packOrUnpack_ = function(method){
+	return function(o, schema){
+		var wasArray = isArray(o),
+			result = goog.array.concat(arr, o),
+			path = goog.array.concat(arr, schema),
+			i, length = path.length;
+		for(i=0;i<length;++i){
+			//result = map.call(result, method, path[i].split("."));
+			result = goog.array.map(result, method, path[i].split("."));
+		}
+		return wasArray ? result : result[0];
+	};
+};
 
-    // called per each schema (pack|unpack)ing each schema
-    function packOrUnpack(method) {
-        return function parse(o, schema) {
-            for (var
-                wasArray = isArray(o),
-                result = concat.call(arr, o),
-                path = concat.call(arr, schema),
-                i = 0, length = path.length;
-                i < length; ++i
-            ) {
-                result = map.call(result, method, path[i].split("."));
-            }
-            return wasArray ? result : result[0];
-        };
-    }
+mt.compression.Jsonh.prototype.pack = function(list, schema){
+	return schema ? 
+		this.packOrUnpack_(this.iteratingWith_(this.hpack_)) : 
+		this.hpack_(list);
+};
 
-    // JSONH.pack
-    function pack(list, schema) {
-        return schema ? packSchema(list, schema) : hpack(list);
-    }
+mt.compression.Jsonh.prototype.unpack = function(hlist, schema){
+	return schema ? 
+		this.packOrUnpack_(this.iteratingWith_(this.hunpack_)) : 
+		this.hunpack_(hlist);
+};
 
-    // JSONH unpack
-    function unpack(hlist, schema) {
-        return schema ? unpackSchema(hlist, schema) : hunpack(hlist);
-    }
+mt.compression.Jsonh.prototype.stringify = function(list, replacer, space, schema){
+	JSON.stringify(this.pack(list, schema), replacer, space);
+};
 
-    // JSON.stringify after JSONH.pack
-    function stringify(list, replacer, space, schema) {
-        return JSON_stringify(pack(list, schema), replacer, space);
-    }
+mt.compression.Jsonh.prototype.parse = function(hlist, reviver, schema){
+	this.unpack(JSON.parse(hlist, reviver), schema);
+};
 
-    // JSONH.unpack after JSON.parse
-    function parse(hlist, reviver, schema) {
-        return unpack(JSON_parse(hlist, reviver), schema);
-    }
-
-    var
-        // recycled for different operations
-        arr = [],
-        // trapped once reused forever
-        concat = arr.concat,
-        // addressed cross platform Object.keys shim
-        Object_keys = Object.keys || function (o) {
-            var keys = [], key;
-            for (key in o) o.hasOwnProperty(key) && keys.push(key);
-            return keys;
-        },
-        // addressed cross platform Array.isArray shim
-        isArray = Array.isArray || (function (toString, arrayToString) {
-            arrayToString = toString.call(arr);
-            return function isArray(o) {
-                return toString.call(o) == arrayToString;
-            };
-        }({}.toString)),
-        // fast and partial Array#map shim
-        map = arr.map || function (callback, context) {
-            for (var
-                self = this, i = self.length, result = Array(i);
-                i--;
-                result[i] = callback.call(context, self[i], i, self)
-            );
-            return result;
-        },
-        // schema related (pack|unpack)ing operations
-        packSchema = packOrUnpack(iteratingWith(hpack)),
-        unpackSchema = packOrUnpack(iteratingWith(hunpack)),
-        // JSON object shortcuts
-        JSON_stringify = JSON.stringify,
-        JSON_parse = JSON.parse
-    ;
-	
-	return {
-        pack: pack,
-        parse: parse,
-        stringify: stringify,
-        unpack: unpack
-    };
-}
-
-
-/*
-// both UPPER and lower, you choose
-var JSONH, jsonh = JSONH = function (Array, JSON) {"use strict"; // if you want
-
-    
-
-    // transforms [{a:"A"},{a:"B"}] to [1,"a","A","B"]
-    function hpack(list) {
-        for (var
-            length = list.length,
-            // defined properties (out of one object is enough)
-            keys = Object_keys(length ? list[0] : {}),
-            klength = keys.length,
-            // static length stack of JS values
-            result = Array(length * klength),
-            i = 0,
-            j = 0,
-            ki, o;
-            i < length; ++i
-        ) {
-            for (
-                o = list[i], ki = 0;
-                ki < klength;
-                result[j++] = o[keys[ki++]]
-            );
-        }
-        // keys.length, keys, result
-        return concat.call([klength], keys, result);
-    }
-
-    // transforms [1,"a","A","B"] to [{a:"A"},{a:"B"}]
-    function hunpack(hlist) {
-        for (var
-            length = hlist.length,
-            klength = hlist[0],
-            result = Array(((length - klength - 1) / klength) || 0),
-            i = 1 + klength,
-            j = 0,
-            ki, o;
-            i < length;
-        ) {
-            for (
-                result[j++] = (o = {}), ki = 0;
-                ki < klength;
-                o[hlist[++ki]] = hlist[i++]
-            );
-        }
-        return result;
-    }
-
-    // recursive: called via map per each item h(pack|unpack)ing each entry through the schema
-    function iteratingWith(method) {
-        return function iterate(item) {
-            for (var
-                path = this,
-                current = item,
-                i = 0, length = path.length,
-                j, k, tmp;
-                i < length; ++i
-            ) {
-                if (isArray(tmp = current[k = path[i]])) {
-                    j = i + 1;
-                    current[k] = j < length ?
-                        map.call(tmp, through, path.slice(j)) :
-                        method(tmp)
-                    ;
-                }
-                current = current[k];
-            }
-            return item;
-        };
-    }
-
-    // called per each schema (pack|unpack)ing each schema
-    function packOrUnpack(method) {
-        return function parse(o, schema) {
-            for (var
-                wasArray = isArray(o),
-                result = concat.call(arr, o),
-                path = concat.call(arr, schema),
-                i = 0, length = path.length;
-                i < length; ++i
-            ) {
-                result = map.call(result, method, path[i].split("."));
-            }
-            return wasArray ? result : result[0];
-        };
-    }
-
-    // JSONH.pack
-    function pack(list, schema) {
-        return schema ? packSchema(list, schema) : hpack(list);
-    }
-
-    // JSONH unpack
-    function unpack(hlist, schema) {
-        return schema ? unpackSchema(hlist, schema) : hunpack(hlist);
-    }
-
-    // JSON.stringify after JSONH.pack
-    function stringify(list, replacer, space, schema) {
-        return JSON_stringify(pack(list, schema), replacer, space);
-    }
-
-    // JSONH.unpack after JSON.parse
-    function parse(hlist, reviver, schema) {
-        return unpack(JSON_parse(hlist, reviver), schema);
-    }
-
-    var
-        // recycled for different operations
-        arr = [],
-        // trapped once reused forever
-        concat = arr.concat,
-        // addressed cross platform Object.keys shim
-        Object_keys = Object.keys || function (o) {
-            var keys = [], key;
-            for (key in o) o.hasOwnProperty(key) && keys.push(key);
-            return keys;
-        },
-        // addressed cross platform Array.isArray shim
-        isArray = Array.isArray || (function (toString, arrayToString) {
-            arrayToString = toString.call(arr);
-            return function isArray(o) {
-                return toString.call(o) == arrayToString;
-            };
-        }({}.toString)),
-        // fast and partial Array#map shim
-        map = arr.map || function (callback, context) {
-            for (var
-                self = this, i = self.length, result = Array(i);
-                i--;
-                result[i] = callback.call(context, self[i], i, self)
-            );
-            return result;
-        },
-        // schema related (pack|unpack)ing operations
-        packSchema = packOrUnpack(iteratingWith(hpack)),
-        unpackSchema = packOrUnpack(iteratingWith(hunpack)),
-        // JSON object shortcuts
-        JSON_stringify = JSON.stringify,
-        JSON_parse = JSON.parse
-    ;
-
-    return {
-        pack: pack,
-        parse: parse,
-        stringify: stringify,
-        unpack: unpack
-    };
-
-}(Array, JSON);
-
-// export for node.js
-if(typeof module != 'undefined' && module.exports) {
-	module.exports = jsonh;
-}
-*/
